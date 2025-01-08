1935a01f44d75170fd0c8ed7a3339495550127d2
Ashik3031 <144212602+Ashik3031@users.noreply.github.com>
2025-01-09 00:35:18 +0530

    Update server connection

--- server/index.js
+++ server/index.js
@@ -21,9 +21,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+  'http://localhost:5173', // Local development URL
+  'https://academia1-backend.onrender.com', // Production frontend URL
+];
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block the request
+    }
+  },
+  credentials: true, // Allow cookies, authorization headers, etc.
 }));
  
 // apis
